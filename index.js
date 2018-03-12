88eb5586d7c6cffc987d010c22ffd43ba18d971e
Jordan Merrill <amanithought@gmail.com>
2018-03-12 19:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   console.log(`Happy holidays, ${name}!`)
 }
+function happyHolidayTo(holiday, person) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(holiday,daysTo) {
+  console.log(`It's ${holiday}, ${daysTo}!`)
+}

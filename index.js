b297ceb329e17c814fca01b9493953b15e995f0e
Jordan Merrill <amanithought@gmail.com>
2018-03-12 19:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 function happyHolidays() {
-  console.log("Happy holidays!")
+  return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, person) {
   console.log(`Happy ${holiday}, ${person}!`)

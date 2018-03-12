710b03f554d45b7e49fba51f7dd67c0234bd4c05
Jordan Merrill <amanithought@gmail.com>
2018-03-12 19:49:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, person) {
   return `Happy ${holiday}, ${person}!`
 }
-function holidayCountdown(days,holiday) {
+function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`
 }
